d3bf9e5e26f800d99a5cb124e3a62684a0f102ac
Christian J <chrjae@gmail.com>
2011-04-24 19:01:12 -0400

    add |compose|, finally

--- src/monads/cj.clj
+++ src/monads/cj.clj
@@ -6,10 +6,15 @@
   (fn [& x]
     (f1 (apply f2 x))))
 
-;; (defmacro compose [& fs]
-;;   )
+(defmacro compose [& fs]
+  `(right-associate compose-2 ~@fs))
 
-
+(ist ((compose #(/ 1 %) #(+ 1 %)) 10)
+     1/11)
+(ist ((compose #(/ 1 %) #(+ 1 %) #(* 2 %)) 10)
+     1/21)
+(ist (macroexpand-all '(compose a b c))
+     (monads.cj/compose-2 a (monads.cj/compose-2 b c)))
 
 
 ;; (define (transaction:>> a b)
