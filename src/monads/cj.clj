6b684fc9677f2b9b292a5af509ae77ae0655c0d8
Christian J <chrjae@gmail.com>
2011-04-24 19:06:23 -0400

    remove commented Scheme originals

--- src/monads/cj.clj
+++ src/monads/cj.clj
@@ -17,27 +17,12 @@
      (monads.cj/compose-2 a (monads.cj/compose-2 b c)))
 
 
-;; (define (transaction:>> a b)
-;;   (lambda (commit cont)
-;;     (a commit
-;;        (lambda (commit)
-;; 	 (b commit
-;; 	    cont)))))
-
 (defn transaction:>> [a b]
   (fn [commit cont]
     (a commit
        (fn [commit]
          (b commit
             cont)))))
-
-;; (define (transaction:>>= a b)
-;;   (lambda (commit cont)
-;;     (a commit
-;;        (lambda (commit value)
-;; 	 ((b value)
-;; 	  commit
-;; 	  cont)))))
 
 (defn transaction:>>= [a b]
   (fn [commit cont]
@@ -46,11 +31,6 @@
          ((b value)
           commit
           cont)))))
-
-;; (define (transaction:return val)
-;;   (lambda (commit cont)
-;;     (cont commit
-;; 	  val)))
 
 (defn transaction:return [val]
   (fn [commit cont]
