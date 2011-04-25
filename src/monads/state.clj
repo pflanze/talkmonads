3f4c5310f9eac9cff0c56f3ed42d44139660f83b
Christian J <chrjae@gmail.com>
2011-04-25 01:45:55 -0400

    move separate namespace parts to separate file

---
+++ src/monads/state.clj
@@ -0,0 +1,39 @@
+(ns monads.state)
+
+;; the monadic type (?) is: (state cont) -> no return (CPS), cont is
+;; receiving the new state [[[and new cont.  NO  hm ? ]]]
+
+;; cont in the case of >> receives just state
+
+(defn >> [a b]
+  (fn [state cont]
+    (a state
+       (fn [state]
+         (b state
+            cont)))))
+
+;; cont in the case of >>= receives [state value]
+;; ç
+
+(defn >>= [a b]
+  (fn [state cont]
+    (a state
+       (fn [state value]
+         ((b value)
+          state
+          cont)))))
+
+(defn return [val]
+  (fn [state cont]
+    (cont state
+          val)))
+
+(defn state-ref []
+  (fn [state cont]
+    (cont state)))
+
+(defn state-set [x]
+  ;; with or without "!"?
+  (fn [state cont]
+    (cont x)))
+
